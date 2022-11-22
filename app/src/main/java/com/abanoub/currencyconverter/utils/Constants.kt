91a7a119e794a97df74d0cf27090995f64d5dc23
Abanoub Samir <abanoub.sameer.22@gmail.com>
2022-11-22 20:49:15 +0200

    Create Constants Object

---
+++ app/src/main/java/com/abanoub/currencyconverter/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.abanoub.currencyconverter.utils
+
+object Constants {
+    const val BASE_URL = "https://api.apilayer.com/exchangerates_data/"
+    const val API_KEY = "ADD_YOUR_OWN_API_KEY_HERE :)"
+}
